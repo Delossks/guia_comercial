ebd9ec9826b31bede6599d54fc376c8c8e632336
Delossks <delossks@gmail.com>
2021-07-18 13:52:07 +0200

    corrección-búsqueda-público
    Se corrige la visualización de la búsqueda de comercio para el usuario público

--- src/Controller/PrincipalController.php
+++ src/Controller/PrincipalController.php
@@ -40,14 +40,220 @@ class PrincipalController extends AbstractController
         $cp_comercio = $request->request->get('codigo_postal');
         $telefono_comercio = $request->request->get('telefono_comercio');
 
-        //Buscar todos los comercios
-        $comerciosTemp = $em->getRepository(Comercio::class)->buscarComercios();
+        //Búsquedas según los parámetros introducidos
+        if(!(empty($nombre_comercio)) && !empty($direccion_comercio) && !empty($codigo_postal) && !empty($telefono_comercio)){
+            //Buscar Comercio por nombre, dirección, código postal y teléfono
+            $comerciosTemp = $em->getRepository(Comercio::class)->createQueryBuilder('c')
+                                                              ->where('c.nombre_comercio LIKE :nombre')
+                                                              ->andWhere('c.direccion_comercio LIKE :direccion')
+                                                              ->andWhere('c.codigo_postal LIKE :codigo_postal')
+                                                              ->andWhere('c.telefono_empresa LIKE :telefono')
+                                                              ->orderBy('c.nombre_comercio', 'ASC')
+                                                              ->setParameter('nombre','%'.$nombre_comercio.'%')
+                                                              ->setParameter('direccion','%'.$direccion_comercio.'%')
+                                                              ->setParameter('cp','%'.$codigo_postal.'%')
+                                                              ->setParameter('telefono','%'.$telefono_comercio.'%')
+                                                              ->getQuery()
+                                                              ->getResult();
+        }
+
+        elseif(!(empty($nombre_comercio)) && !empty($direccion_comercio) && !empty($codigo_postal) && empty($telefono_comercio)){
+            //Buscar Comercio por nombre, dirección y código postal
+            $comerciosTemp = $em->getRepository(Comercio::class)->createQueryBuilder('c')
+                                                              ->where('c.nombre_comercio LIKE :nombre')
+                                                              ->andWhere('c.direccion_comercio LIKE :direccion')
+                                                              ->andWhere('c.codigo_postal LIKE :codigo_postal')
+                                                              ->orderBy('c.nombre_comercio', 'ASC')
+                                                              ->setParameter('nombre','%'.$nombre_comercio.'%')
+                                                              ->setParameter('direccion','%'.$direccion_comercio.'%')
+                                                              ->setParameter('cp','%'.$codigo_postal.'%')
+                                                              ->getQuery()
+                                                              ->getResult();
+        }
+
+        elseif(!(empty($nombre_comercio)) && !empty($direccion_comercio) && empty($codigo_postal) && !empty($telefono_comercio)){
+            //Buscar Comercio por nombre, dirección y teléfono
+            $comerciosTemp = $em->getRepository(Comercio::class)->createQueryBuilder('c')
+                                                              ->where('c.nombre_comercio LIKE :nombre')
+                                                              ->andWhere('c.direccion_comercio LIKE :direccion')
+                                                              ->andWhere('c.telefono_empresa LIKE :telefono')
+                                                              ->orderBy('c.nombre_comercio', 'ASC')
+                                                              ->setParameter('nombre','%'.$nombre_comercio.'%')
+                                                              ->setParameter('direccion','%'.$direccion_comercio.'%')
+                                                              ->setParameter('telefono','%'.$telefono_comercio.'%')
+                                                              ->getQuery()
+                                                              ->getResult();
+        }
+
+        elseif(!(empty($nombre_comercio)) && empty($direccion_comercio) && !empty($codigo_postal) && !empty($telefono_comercio)){
+            //Buscar Comercio por nombre, código postal y teléfono
+            $comerciosTemp = $em->getRepository(Comercio::class)->createQueryBuilder('c')
+                                                              ->where('c.nombre_comercio LIKE :nombre')
+                                                              ->andWhere('c.codigo_postal LIKE :codigo_postal')
+                                                              ->andWhere('c.telefono_empresa LIKE :telefono')
+                                                              ->orderBy('c.nombre_comercio', 'ASC')
+                                                              ->setParameter('nombre','%'.$nombre_comercio.'%')
+                                                              ->setParameter('cp','%'.$codigo_postal.'%')
+                                                              ->setParameter('telefono','%'.$telefono_comercio.'%')
+                                                              ->getQuery()
+                                                              ->getResult();
+        }
+
+        elseif((empty($nombre_comercio)) && !empty($direccion_comercio) && !empty($codigo_postal) && !empty($telefono_comercio)){
+            //Buscar Comercio por dirección, código postal y teléfono
+            $comerciosTemp = $em->getRepository(Comercio::class)->createQueryBuilder('c')
+                                                              ->where('c.direccion_comercio LIKE :direccion')
+                                                              ->andWhere('c.codigo_postal LIKE :codigo_postal')
+                                                              ->andWhere('c.telefono_empresa LIKE :telefono')
+                                                              ->orderBy('c.nombre_comercio', 'ASC')
+                                                              ->setParameter('direccion','%'.$direccion_comercio.'%')
+                                                              ->setParameter('cp','%'.$codigo_postal.'%')
+                                                              ->setParameter('telefono','%'.$telefono_comercio.'%')
+                                                              ->getQuery()
+                                                              ->getResult();
+        }
+
+        elseif(!(empty($nombre_comercio)) && !empty($direccion_comercio) && empty($codigo_postal) && empty($telefono_comercio)){
+            //Buscar Comercio por nombre y dirección
+            $comerciosTemp = $em->getRepository(Comercio::class)->createQueryBuilder('c')
+                                                              ->where('c.nombre_comercio LIKE :nombre')
+                                                              ->andWhere('c.direccion_comercio LIKE :direccion')
+                                                              ->orderBy('c.nombre_comercio', 'ASC')
+                                                              ->setParameter('nombre','%'.$nombre_comercio.'%')
+                                                              ->setParameter('direccion','%'.$direccion_comercio.'%')
+                                                              ->getQuery()
+                                                              ->getResult();
+        }
+
+        elseif(!(empty($nombre_comercio)) && empty($direccion_comercio) && !empty($codigo_postal) && empty($telefono_comercio)){
+            //Buscar Comercio por nombre y código postal
+            $comerciosTemp = $em->getRepository(Comercio::class)->createQueryBuilder('c')
+                                                              ->where('c.nombre_comercio LIKE :nombre')
+                                                              ->andWhere('c.codigo_postal LIKE :codigo_postal')
+                                                              ->orderBy('c.nombre_comercio', 'ASC')
+                                                              ->setParameter('nombre','%'.$nombre_comercio.'%')
+                                                              ->setParameter('cp','%'.$codigo_postal.'%')
+                                                              ->getQuery()
+                                                              ->getResult();
+        }
+
+        elseif((empty($nombre_comercio)) && !empty($direccion_comercio) && !empty($codigo_postal) && empty($telefono_comercio)){
+            //Buscar Comercio por dirección y código postal
+            $comerciosTemp = $em->getRepository(Comercio::class)->createQueryBuilder('c')
+                                                              ->andWhere('c.direccion_comercio LIKE :direccion')
+                                                              ->andWhere('c.codigo_postal LIKE :codigo_postal')
+                                                              ->andWhere('c.telefono_empresa LIKE :telefono')
+                                                              ->orderBy('c.nombre_comercio', 'ASC')
+                                                              ->setParameter('direccion','%'.$direccion_comercio.'%')
+                                                              ->setParameter('cp','%'.$codigo_postal.'%')
+                                                              ->getQuery()
+                                                              ->getResult();
+        }
+
+        elseif(!(empty($nombre_comercio)) && empty($direccion_comercio) && empty($codigo_postal) && !empty($telefono_comercio)){
+            //Buscar Comercio por nombre y teléfono
+            $comerciosTemp = $em->getRepository(Comercio::class)->createQueryBuilder('c')
+                                                              ->where('c.nombre_comercio LIKE :nombre')
+                                                              ->andWhere('c.telefono_empresa LIKE :telefono')
+                                                              ->orderBy('c.nombre_comercio', 'ASC')
+                                                              ->setParameter('nombre','%'.$nombre_comercio.'%')
+                                                              ->setParameter('telefono','%'.$telefono_comercio.'%')
+                                                              ->getQuery()
+                                                              ->getResult();
+        }
+
+        elseif((empty($nombre_comercio)) && !empty($direccion_comercio) && empty($codigo_postal) && !empty($telefono_comercio)){
+            //Buscar Comercio por dirección y teléfono
+            $comerciosTemp = $em->getRepository(Comercio::class)->createQueryBuilder('c')
+                                                              ->andWhere('c.direccion_comercio LIKE :direccion')
+                                                              ->andWhere('c.telefono_empresa LIKE :telefono')
+                                                              ->orderBy('c.nombre_comercio', 'ASC')
+                                                              ->setParameter('direccion','%'.$direccion_comercio.'%')
+                                                              ->setParameter('telefono','%'.$telefono_comercio.'%')
+                                                              ->getQuery()
+                                                              ->getResult();
+        }
+
+        elseif((empty($nombre_comercio)) && empty($direccion_comercio) && !empty($codigo_postal) && !empty($telefono_comercio)){
+            //Buscar Comercio por código postal y teléfono
+            $comerciosTemp = $em->getRepository(Comercio::class)->createQueryBuilder('c')
+                                                              ->andWhere('c.codigo_postal LIKE :codigo_postal')
+                                                              ->andWhere('c.telefono_empresa LIKE :telefono')
+                                                              ->orderBy('c.nombre_comercio', 'ASC')
+                                                              ->setParameter('cp','%'.$codigo_postal.'%')
+                                                              ->setParameter('telefono','%'.$telefono_comercio.'%')
+                                                              ->getQuery()
+                                                              ->getResult();
+        }
+
+        elseif(!(empty($nombre_comercio)) && empty($direccion_comercio) && empty($codigo_postal) && empty($telefono_comercio)){
+            //Buscar Comercio por nombre
+            $comerciosTemp = $em->getRepository(Comercio::class)->createQueryBuilder('c')
+                                                              ->where('c.nombre_comercio LIKE :nombre')
+                                                              ->orderBy('c.nombre_comercio', 'ASC')
+                                                              ->setParameter('nombre','%'.$nombre_comercio.'%')
+                                                              ->getQuery()
+                                                              ->getResult();
+        }
+
+        elseif((empty($nombre_comercio)) && !empty($direccion_comercio) && empty($codigo_postal) && empty($telefono_comercio)){
+            //Buscar Comercio por dirección
+            $comerciosTemp = $em->getRepository(Comercio::class)->createQueryBuilder('c')
+                                                              ->andWhere('c.direccion_comercio LIKE :direccion')
+                                                              ->orderBy('c.nombre_comercio', 'ASC')
+                                                              ->setParameter('direccion','%'.$direccion_comercio.'%')
+                                                              ->getQuery()
+                                                              ->getResult();
+        }
+
+        elseif((empty($nombre_comercio)) && empty($direccion_comercio) && !empty($codigo_postal) && empty($telefono_comercio)){
+            //Buscar Comercio por código postal
+            $comerciosTemp = $em->getRepository(Comercio::class)->createQueryBuilder('c')
+                                                              ->andWhere('c.codigo_postal LIKE :codigo_postal')
+                                                              ->orderBy('c.nombre_comercio', 'ASC')
+                                                              ->setParameter('cp','%'.$codigo_postal.'%')
+                                                              ->getQuery()
+                                                              ->getResult();
+        }
+
+        elseif((empty($nombre_comercio)) && empty($direccion_comercio) && empty($codigo_postal) && !empty($telefono_comercio)){
+            //Buscar Comercio por teléfono
+            $comerciosTemp = $em->getRepository(Comercio::class)->createQueryBuilder('c')
+                                                              ->andWhere('c.telefono_empresa LIKE :telefono')
+                                                              ->orderBy('c.nombre_comercio', 'ASC')
+                                                              ->setParameter('telefono','%'.$telefono_comercio.'%')
+                                                              ->getQuery()
+                                                              ->getResult();
+        }
+
+        else {
+            //Buscar todos los comercios
+            $comerciosTemp = $em->getRepository(Comercio::class)->findBy(array(), array('nombre_comercio' => 'ASC'));
+            //$comerciosTemp = $em->getRepository(Comercio::class)->buscarComercios();
+        }
 
         $comercios = $comerciosTemp;
 
         return $this->render('principal/buscarComercio.html.twig', [
             'controller_name' => 'Esta es la página para buscar un Comercio',
             'comercios' => $comercios
+        ]);
+    }
+
+    #[Route('/comercio/consultar', name: 'consultarComercio')]
+    public function consultarComercio(Request $request, $id): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        //Buscar el comercio a consultar
+        $comercio = $em->getRepository(Comercio::class)->findOneBy(array('id' => $id));
+
+        $form = $this->createForm(ComercioType::class, $comercio);
+
+        return $this->render('principal/consultarComercio.html.twig', [
+            'controller_name' => 'Datos del comercio',
+            'formulario' => $form->createView(),
+            'comercio' => $comercio
         ]);
     }
 }
